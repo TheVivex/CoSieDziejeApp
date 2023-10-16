38e1ac33f0f2409bad64d504129f14f431573de1
Bartosz Brzostek <brzostek.bartosz@wp.pl>
2023-10-16 18:15:42 +0200

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WRWpr
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WRWpr
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
