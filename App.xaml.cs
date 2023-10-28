9b4b785c40cfcde212c521007bbd3e0ad1c01e8b
woznin <szymonw037@gmail.com>
2023-10-28 12:05:52 +0200

    Podstawowe mapy

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,7 @@
     {
         public App()
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpQXxbf1xzZFBMZFpbQHdPMyBoS35RdURjW3dedXBTQmFaWEBz");
             InitializeComponent();
 
             MainPage = new AppShell();
