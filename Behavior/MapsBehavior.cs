62a41cb338610e1a2e88b263250ff219ccfc7c49
woznin <szymonw037@gmail.com>
2023-10-28 15:39:14 +0200

    Klikalność mapy

---
+++ Behavior/MapsBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.Maui.Maps;
+
+namespace WRWpr
+{
+    public class MapsBehavior : Behavior<ContentPage>
+    {
+        private MapTileLayer mapa;
+        private MapMarker marker;
+
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.mapa = bindable.FindByName<MapTileLayer>("mapa");
+            this.mapa.Tapped += TileLayer_Tapped;
+            this.marker = bindable.FindByName<MapMarker>("marker");
+        }
+
+        private void TileLayer_Tapped(object sender, Syncfusion.Maui.Maps.TappedEventArgs e)
+        {
+            var location = mapa.GetLatLngFromPoint(e.Position);
+            marker.Latitude = location.Latitude;
+            marker.Longitude = location.Longitude;
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            if (this.mapa != null)
+            {
+                this.mapa.Tapped -= TileLayer_Tapped;
+            }
+
+            this.mapa = null;
+            this.marker = null;
+        }
+    }
+}
